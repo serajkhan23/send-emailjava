7cf305dc677a99b247d20f78a6ded78cb87cf8d0
serajkhan23 <mdserajkhan2000@gmail.com>
2023-06-11 21:34:25 +0530

    send email through java

---
+++ README.md
@@ -0,0 +1,1 @@
+This si for send email through java
---
+++ src/main/java/Geekster/Main.java
@@ -0,0 +1,13 @@
+package Geekster;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Starting to send out Mail");
+
+        MailHandler mailHandler = new MailHandler();
+        mailHandler.sendMail();
+        }
+}
